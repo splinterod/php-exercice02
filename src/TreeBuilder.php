39108ee092167e269d2f32c00e1b1b54839fef42
julien <julienl6900669006@gmail.com>
2020-04-28 13:38:11 +0200

    ajout création tableau multidimensionnel et ajout de tests

--- src/TreeBuilder.php
+++ src/TreeBuilder.php
@@ -14,12 +14,48 @@ class TreeBuilder
 
     public function build()
     {
-        // fill in this method
-        // you can create other methods to organize your code.
+        $output = [];
 
-        // @todo
+        // on parcours toutes les lignes du tableau initial
+        foreach ($this->flatData as $title) {
+            // extraction des numéros de chapitre avec une expression réguliere: $chapterNumbers est un array.
+            //on peux gérer jusqu'au chapitre 999.999.
+            preg_match_all('~\d{1,3}.~', $title, $chapterNumbers);
 
-        return $this;
+            // Permet de connaitre la "profondeur" du chapitre :
+            // si  la taille de $chapterNumbers = 1  alors chapitre niveau 1.
+            // Si la taille de $chapterNumbers = 2 alors chapitre de niveau 2 (enfants d'un chapitre de niveau 1)...
+            $indexSize = count($chapterNumbers[0]);
+
+            // en fonction de la "profondeur" du titre de chapitre en cours d'analyse, on le positionne dans le tableau de sortie en fct de son niveau hierarchique.
+            // On ajoute directement le titre et l'index au bon endroit dans le tableau ( les clés et la valeur "index" sont les memes ): On n'a pas à se soucier de l'ordre
+            // lequel les titres sont classés dans le tableau initial.
+            switch ($indexSize) {
+                case 1:
+                    // extraction du numéro de chapitre ( sous forme d. ) issu du preg_match_all
+                    $level1 = filter_var($chapterNumbers[0][0][0], FILTER_SANITIZE_NUMBER_INT);
+                    //  on ajoute titre et index au tableau (à voir si la conservation de la valeur "index" est nécessaire)
+//                Avant l'ajout , on pourrais tester si la valeur existe daje
+
+                    $output[$level1]["index"] = $level1;
+//                     on ajoute le titre en enlevant les 3 premiers caractères correspondant au numéro de chapitre (<number><dot><space>)
+                    $output[$level1]["title"] = substr($title,3);
+//
+                    break;
+                case 2:
+                    $level1 = filter_var($chapterNumbers[0][0][0], FILTER_SANITIZE_NUMBER_INT);
+                    $level2 = filter_var($chapterNumbers[0][1][0], FILTER_SANITIZE_NUMBER_INT);
+                    $output[$level1]["children"][$level2]["index"] = $level2;
+                    //on ajoute le titre en enlever les 5 premiers caractères correspondant au numéro de chapitre (<number><dot><number><dot><space>)
+                    $output[$level1]["children"][$level2]["title"] = substr($title,5);
+                    break;
+                default:
+                    throw new Exception('le nombre de niveau hierarchique max (2 niveaux) est atteint');
+            }
+        }
+        $this->tree = $output;
+
+        return $this->tree;
     }
 
     public function getTree()
--- tests/TreeBuilderTest.php
+++ tests/TreeBuilderTest.php
@@ -20,10 +20,31 @@ final class TreeBuilderTest extends TestCase
         $tree = new TreeBuilder($input);
         $tree->build();
         $nestedTree = $tree->getTree();
-        
+
         // complete the unit test here...
         // make sure the top items number is correct
         // make sure nested item values are correct
+
+        //Création du tableau initial à partir  du tableau associatif
+        $originalTable = [];
+
+//       On parcours le tableau associatif
+        Foreach($nestedTree as  $chapter){
+//            niveau hiearchique 1: on récupére l'index (numéro de chapitre) et on concatene avec le titre du chapitre
+            array_push($originalTable, $chapter['index'] . ". " . $chapter['title']);
+//           on vérifie si un niveau hierarchique 2 existe
+            if(isset($chapter['children'])){
+//                on parcours l'ensemble des sous chapitre de niveau 2
+                Foreach($chapter['children'] as $subChpater){
+//                    ajoute au tableau normal chaques numéro de chapitre (en reprenant aussi le numero index niveau 1) et
+//                    on concatene avec le titre du sous chapitre
+                    array_push($originalTable, $chapter['index'] . ".d" . $subChpater['index'] . ". ". $subChpater['title']);
+                }
+
+            }
+        }
+
+        $this->assertEquals($input,$originalTable);
 
         $this->assertCount(4, $nestedTree);
         // @todo: other simple tests to verify sub items...
