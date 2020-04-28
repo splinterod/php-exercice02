7dcaf96840c3477612495491f35f0f2e48a1256a
julien <julienl6900669006@gmail.com>
2020-04-28 19:12:00 +0200

    code revu et commentaires corrigés sans bonus

--- src/TreeBuilder.php
+++ src/TreeBuilder.php
@@ -18,7 +18,7 @@ class TreeBuilder
     {
         /*      tri du tableau initial afin de respecter l'ordre des chapitres.
                le tableau en sortie sera ainsi trié dans l'ordre des chapitres et
-               on cela évitera de compléter ce tableau de facon aléatoire */
+               cela évitera de compléter ce tableau de façon aléatoire */
         sort($this->flatData);
 
         $nestedTable = [];
@@ -36,13 +36,13 @@ class TreeBuilder
 
             /* en fonction de la "profondeur" du titre de chapitre en cours d'analyse, on le positionne dans le tableau de sortie en fct de son niveau hierarchique.
              On ajoute directement le titre et l'index au bon endroit dans le tableau ( les clés et la valeur "index" sont les memes ): On n'a pas à se soucier de l'ordre
-             lequel les titres sont classés dans le tableau initial. D'autant plus que la source a été trié */
+             lequel les titres sont classés dans le tableau initial. D'autant plus que la source a été triée */
             switch ($indexSize) {
                 case 1:
                     // extraction du numéro de chapitre ( sous forme d. ) issu du preg_match_all
                     $level1 = filter_var($chapterNumbers[0][0][0], FILTER_SANITIZE_NUMBER_INT);
-                    /* on ajoute titre et index au tableau (à voir si la conservation de la valeur "index" est nécessaire)
-                   Avant l'ajout , on pourrais tester si la valeur existe déja*/
+                    /* on ajoute titre et index au tableau
+                   Avant l'ajout , on pourrait tester si la valeur existe déja et remonter une erreur de numérotation*/
                     $nestedTable[$level1]["index"] = $level1;
 //                     on ajoute le titre en enlevant les 3 premiers caractères correspondant au numéro de chapitre (<number><dot><space>)
                     $nestedTable[$level1]["title"] = substr($title, 3);
