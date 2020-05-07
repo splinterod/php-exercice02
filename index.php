8be80990e5d77580de5e756edaabae8610f09746
julien <julienl6900669006@gmail.com>
2020-05-07 20:07:36 +0200

    correction et ajout du tests pour validation si nb de sous chapitre infini

--- index.php
+++ index.php
@@ -3,20 +3,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 //ajout initialisation xdebug pour affichage complet des var_dump
-ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_depth', '20');
 ini_set('xdebug.var_display_max_children', '256');
 ini_set('xdebug.var_display_max_data', '1024');
 error_reporting(E_ALL);
 
 require "vendor/autoload.php";
 
-use App\TreeBuilder;
+use App\TreeBuilderInfinite;
 
 $input = [
 
-   '1. Dénomination du médicament',
-   '2. Effets indésirables',
+    '1. Dénomination du médicament',
+    '1.1. tests niveau un un',
+    '1.2. tests niveau un deux',
+    '1.2.3 test pour niveau un deux trois',
+    '1.2.3.4.5.6.7.8.9. test un deux trois quatre cinq six',
+    '2. Effets indésirables',
     '2.1. Effet sur la grossesse',
+    '2.1.1. Effet sur la enfants',
     '3. Forme pharmaceutique',
     '3.1. Contre indications',
     '4. Données pharmaceutiques',
@@ -24,6 +29,6 @@ $input = [
 
 ];
 
-$tree = new TreeBuilder($input);
+$tree = new TreeBuilderInfinite($input);
 $tree->build();
 var_dump($tree->getTree());
--- src/TreeBuilderInfinite.php
+++ src/TreeBuilderInfinite.php
@@ -15,69 +15,38 @@ class TreeBuilderInfinite
     public function build()
     {
         sort($this->flatData);
-
-        // Tests pour une nombre infini hierarchique
-        $this->tree = $this->createNestedTable();
-
+        $this->tree = $this->createdNestedTable();
 
         return $this->tree;
     }
 
-    public function createNestedTable()
-    {
+    public function createdNestedTable() {
 
         $nestedTable = [];
 
-
-        //On parcours toutes lignes du tableau
         foreach ($this->flatData as $title) {
             preg_match_all('~\d{1,3}.~', $title, $chapterNumbers);
-            // on va créer le tableau à ajouter en sens inverse, on inverse l'ordre des numéros de chapitre
             $chapterNumbers = array_reverse($chapterNumbers[0]);
+            $arrayToAdd = [];
 
-            // on reset le tableau qu'on va ajouter
-            $arraytoAdd = [];
-
-            // On ajoute le titre le dernier numéro de chapitre ainsi que le titre (donc 0 car $chapter est inversé)
-            $arraytoAdd[filter_var($chapterNumbers[0], FILTER_SANITIZE_NUMBER_INT)] = [
-                'index' => filter_var($chapterNumbers[0], FILTER_SANITIZE_NUMBER_INT),
+            $arrayToAdd[str_replace('.','',$chapterNumbers[0])] = [
+                'index' => str_replace('.','',$chapterNumbers[0]),
                 'title' => $title,
             ];
 
-            // on reset le tableau qu'on va ajouter au tableau final
-            $arrayForTable = [];
-
-             /* On crée la profondeur (en fct de la longueur du tableau $chapter sauf pour chapterNumber[0]
-            deja traité au dessus et si count($chapterNumber = 1 , on fait rien)*/
             for ($i = 1; $i < count($chapterNumbers); $i++) {
-               /* premiere boucle on ajoute le tableau $arrayToAdd sinon on ajoute le arrayFor Table sur lui meme . En conservant les index et clés pour faciliter
-                le merge entre les tableaux*/
-                if ($i == 1) {
-                    $arrayForTable[filter_var($chapterNumbers[$i], FILTER_SANITIZE_NUMBER_INT)] = [
-                        'index' => filter_var($chapterNumbers[$i], FILTER_SANITIZE_NUMBER_INT),
-                        'title' => '',
-                        'children' => $arraytoAdd
-                    ];
-                } else {
-                    $arrayForTable[filter_var($chapterNumbers[$i], FILTER_SANITIZE_NUMBER_INT)] = [
-                        'index' => filter_var($chapterNumbers[$i], FILTER_SANITIZE_NUMBER_INT),
-                        'title' => '',
-                        'children' => $arrayForTable
-                    ];
-                }
+                $arrayChildren = $arrayToAdd;
+                $arrayToAdd =[];
+                $arrayToAdd[str_replace('.','',$chapterNumbers[$i])] = [
+                    'index' => str_replace('.','',$chapterNumbers[$i]),
+                    'title' => '',
+                    'children' => $arrayChildren
+                ];
             }
 
-//            si arrayForTable = empty alors on est pas rentré dasn la boucle donc arrayForTable est vide
-            $arrayForTable = (empty($arrayForTable)) ? $arraytoAdd : $arrayForTable;
-
-              /*Le sens des arguments dans la fonction array_replace_recursive est important ici. On prend le tableau avec les nouvelles valeurs et on le complète
-            avec  toutes les infos de nestedTable et pas l'inverse->perte de données*/
-            $nestedTable = array_replace_recursive($arrayForTable, $nestedTable);
+            $nestedTable = array_replace_recursive($arrayToAdd, $nestedTable);
             asort($nestedTable);
-
         }
-        //affichage du résultat :
-        var_dump($nestedTable);
         return $nestedTable;
     }
 
