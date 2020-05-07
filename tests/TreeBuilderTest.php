309d2df0f9dd72220e3bcb8d1caa74202200ce97
julien <julienl6900669006@gmail.com>
2020-05-07 19:52:34 +0200

    correction et ajout du tests pour validation si nb de sous chapitre infini

--- tests/TreeBuilderTest.php
+++ tests/TreeBuilderTest.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\TreeBuilderInfinite;
 use PHPUnit\Framework\TestCase;
-use App\TreeBuilder;
 
 final class TreeBuilderTest extends TestCase
 {
@@ -9,50 +9,37 @@ final class TreeBuilderTest extends TestCase
     {
         $input = [
             '1. Dénomination du médicament',
+            '1.1. tests niveau un un',
+            '1.2. tests niveau un deux',
+            '1.2.3 test pour niveau un deux trois',
+            '1.2.3.4.5.6. test un deux trois quatre cinq six',
             '2. Effets indésirables',
             '2.1. Effet sur la grossesse',
+            '2.1.1. Effet sur la enfants',
             '3. Forme pharmaceutique',
             '3.1. Contre indications',
             '4. Données pharmaceutiques',
             '4.1. Données de sécurité préclinique',
-            '4.2. Données test niveau 4',
-            '4.3. Données test',
-            '4.4. Données test',
         ];
 
         //on trie ici input car il sera trié dans la méthode getTree() donc nécessaire pour valider si les deux tableaux sont identiques
         sort($input);
 
-        $tree = new TreeBuilder($input);
+        $tree = new TreeBuilderInfinite($input);
         $tree->build();
         $nestedTree = $tree->getTree();
 
-//        Pour effectuer un test aussi bien sur la numérotation que que les titres, l'idée est de récréer le tableau d'origine
-//        en partant du tableau assocatif: et si il y a un ecart -> il y a une erreur.
-//        ce test est valable pour un niveau de hierarchie max de 2
-
-        //Création du tableau d'origine  à partir  du tableau associatif
-        $origineTable = [];
-
-//       On parcours le tableau associatif
-        Foreach($nestedTree as  $chapter){
-/*          Niveau hiearchique 1: on récupére l'index (numéro de chapitre) et on concatène avec le titre du chapitre
-            en respectant le format "<number><dot> NOM CHAPITRE"*/
-            array_push($origineTable, $chapter['index'] . ". " . $chapter['title']);
-//           on vérifie si un niveau hierarchique niveau 2 existe dans le tableau associatif
-            if(isset($chapter['children'])){
-//                si oui , on parcours l'ensemble des sous chapitre de niveau 2
-                Foreach($chapter['children'] as $subChapter){
-/*                  Ajoute au tableau chaque numéro de chapitre (en reprenant aussi le numéro index niveau 1) et
-                    on concatène avec le titre du sous chapitre  en respectant le format "<number><dot><number><dot> NOM CHAPITRE"*/
-                    array_push($origineTable, $chapter['index'] . "." . $subChapter['index'] . ". ". $subChapter['title']);
+        $it = new RecursiveIteratorIterator(new RecursiveArrayIterator($nestedTree));
+        foreach($it as $k => $v) {
+            if($v!= ''){
+                if($k==='title'){
+                    $origineTable[]= $v;
                 }
             }
         }
-//        vérification si le tableau initial (trié) est identique au tableau créé à partir du tableau associatif
+        sort($origineTable);
         $this->assertEquals($input,$origineTable);
 
-        $this->assertCount(4, $nestedTree);
         // @todo: other simple tests to verify sub items...
     }
 }
